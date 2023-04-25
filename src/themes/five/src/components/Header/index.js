d10f53ebd22e6fae3c4162c247fc11602ddb600e
Carlos Quiroz <carlos.q@ordering.co>
2023-04-24 20:52:53 -0400

    update version 4.0.0

--- src/themes/five/src/components/Header/index.js
+++ src/themes/five/src/components/Header/index.js
@@ -332,7 +332,9 @@ export const Header = (props) => {
               placeholder={t('SEARCH_BUSINESSES', 'Search Businesses')}
               starbucksStyle
               onSearch={(value) => setSearchValue(value)}
-              handleCustomEnter={() => events.emit('go_to_page', { page: 'business_search' })}
+              handleCustomEnter={() => configState?.configs?.advanced_business_search_enabled?.value === '0' || businessSlug
+                ? null
+                : events.emit('go_to_page', { page: 'business_search' })}
             />
           </HeaderSearchMode>
         )}
